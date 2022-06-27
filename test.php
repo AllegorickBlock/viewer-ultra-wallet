478905e9b3f3332c9d1fd3525ab03c8b072041ce
la170005 <la170005@student.helha.be>
2022-06-28 00:07:50 +0200

    select the token factory

--- test.php
+++ test.php
@@ -16,8 +16,68 @@
     $strJsonFileContents = file_get_contents("unzip/manifest.json");
     //echo $strJsonFileContents;
 	$json = json_decode($strJsonFileContents,true);
-	echo $json['media']['images']['product'];
+	//echo $json['media']['images']['product'];
+
+	$url = 'https://api.ultra.eossweden.org/v1/chain/get_table_rows';
+
+	$data = [
+		"code"=> "eosio.nft.ft",
+    	"table"=> "factory.a",
+    	"scope"=> "eosio.nft.ft",
+    	"json"=> true,
+    	"limit"=> 1
+	];
+
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS,  json_encode($data));
+	curl_setopt($curl, CURLOPT_HTTPHEADER, [
+  'Content-Type: application/json'
+]);
+	$response = curl_exec($curl);
+	curl_close($curl);
+	echo $response;
+?>	
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Futur Elon musk</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  </head>
+  <body>
+    <img src="<?php echo 'unzip/'.$json['media']['images']['product'];?>" width="200" height="200">
+  </body>
+</html>
+<script>
+	$.ajax({
+		type: 'POST',
+		url:  'https://ultra.eosrio.io/v1/chain/get_table_rows',
+		dataType: 'json',
+		data: JSON.stringify({
+			    "code": "eosio.nft.ft",
+			    "table": "factory.a",
+			    "scope": "eosio.nft.ft",
+			    "json": true,
+			    "limit": 1
+			}),
+		}).done(function(data){
+
+			var url=data['rows'][0]['meta_uris'][0];     
+     		window.open(url, 'Download');
+			// console.log(data['rows'][0]['meta_uris'][0]);
+			// $.ajax({
+			// 	type: 'POST',
+			// 	url:  data['rows'][0]['meta_uris'][0],
+				
+				
+			// 	}).done(function(data){
+			// 		console.log(data);
+			// 		window.location = data;
+			// 	});
+		});
 
 
-?>
-<img src="<?php echo 'unzip/'.$json['media']['images']['product'];?>" width="200" height="200">
+
+</script>
